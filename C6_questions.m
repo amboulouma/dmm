b05a102bdcbeec013ae1f6e4867f4a356f1d9af5
satyamsubhash <49028893+satyamsubhash@users.noreply.github.com>
2021-05-09 18:58:35 +0200

    Update C6_questions.m

--- C6_questions.m
+++ C6_questions.m
@@ -1,1 +1,13 @@
-%% C6.
+%% C6. Verifying the gamma from C5 to checl is it achieve close to the value of highest level of welfare.
+
+
+
+%% Defining a grid for gamma from 0 to 0.02.
+
+
+
+%% Obtaining the equilibrium path and computing household utility as C3(b).
+
+
+
+%% Plotting the utility levels as a function of gamma.
