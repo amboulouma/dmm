4769a742c9d15a8b1a8785514c7fed308551e081
satyamsubhash <49028893+satyamsubhash@users.noreply.github.com>
2021-05-09 18:53:48 +0200

    Update C5_questions.m

--- C5_questions.m
+++ C5_questions.m
@@ -1,8 +1,18 @@
-%% C5.
+%% C5. Assume that the minister of finance wants to use at most 1% of output for testing purposes, i.e gamma = 0.01.
+
+
 
 function P = C5_questions(P)
-% (a) 
+% C5(a) Plotting the equilibrium paths of consumption, capital and labor as functions of time.
 
 P.gamma = 0.01;
+
+
+% C5(b) Plotting the time paths of infected and susceptible individuals.
+
+
+
+% C5(c) Plotting the time paths of the test price p_t, the testing intensity phi_t and the testing expenditure.
+% as share of output.
 
 end
